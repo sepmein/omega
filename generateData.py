1b131bc4896b5a0c966b1f77eaa3fa18d881ee7a
Spencer Zhang <sepmein@SpencerdeiMac.local>
2017-02-26 07:32:38 +0800

    fixed )

--- generateData.py
+++ generateData.py
@@ -3,4 +3,4 @@ from board import Board
 """每个批次10000个"""
 for i in range(20):
     b = Board(4)
-    b.generateAndExportGame(10000, 'result' + str(i)
+    b.generateAndExportGame(10000, 'result' + str(i))

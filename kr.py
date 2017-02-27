c5a7861d1ed1268b491c3a1230061a9e05f0a835
Spencer Zhang <sepmein@gmail.com>
2017-02-27 20:14:41 +0800

    modified model

--- kr.py
+++ kr.py
@@ -4,6 +4,7 @@ data = []
 for i in range(200):
     data.append(np.load('formatted' + str(i) + '.npy'))
 DATASET = np.vstack(stack for stack in data)
+DATASET[:,67][DATASET[:,67] == 0] =2
 training_ratio = 0.7
 training_num = int(training_ratio * DATASET.shape[0])
 TRAINING_SET = DATASET[:training_num]
@@ -13,16 +14,16 @@ from keras.models import Sequential
 
 model = Sequential()
 
-from keras.layers import Dense, Activation
+from keras.layers import LSTM, Dense, Activation, Dropout
 from keras.utils.np_utils import to_categorical
 
 
-model.add(Dense(100, input_dim=67))
-model.add(Activation('relu'))
-model.add(Dense(output_dim=100))
-model.add(Activation('relu'))
-# model.add(Dense(output_dim=256))
-# model.add(Activation('relu'))
+model.add(LSTM(100, input_shape=(None,67)))
+model.add(Activation('tanh'))
+model.add(LSTM(100))
+model.add(Activation('tanh'))
+# model.add(LSTM(output_dim=100))
+# model.add(Activation('tanh'))
 model.add(Dense(output_dim=3))
 model.add(Activation('softmax'))
 
@@ -36,7 +37,7 @@ Y_train = to_categorical(Y_train_int)
 X_test = TEST_SET[:, :67]
 Y_test_int = TEST_SET[:, 67:]
 Y_test = to_categorical(Y_test_int)
-model.fit(X_train, Y_train, nb_epoch=5, batch_size=32)
+model.fit(X_train, Y_train, nb_epoch=6, batch_size=32)
 
 loss_and_metrics = model.evaluate(X_test, Y_test, batch_size=32)
 print(loss_and_metrics)
